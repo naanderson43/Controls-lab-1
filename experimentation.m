a1950a2acc2d3245f815a7b0c96530e480fc03d3
Sikender Shahid <sikendershahid91@gmail.com>
2017-11-14 14:28:28 -0600

    done a b, rest remaining

---
+++ experimentation.m
@@ -0,0 +1,13 @@
+
+K_m = 25; 
+K_b = 0.0185;
+B = 0.3; 
+R_a = 1.25;
+L_a = 0; %approximate to zero
+J_m = 0.01;
+J_l = 6.25;
+
+motor_sys = tf(K_m, [L_a*J_m, L_a*B+R_a*J_m, R_a*B+K_m*K_b]); 
+gear_sys = tf(1, 20); 
+load_sys = tf(1,[1 0]); 
+open_loop_tf = motor_sys * gear_sys * load_sys; 
