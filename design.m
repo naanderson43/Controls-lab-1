f43cc13017e3e4417d3da39e09eb138a43d6a933
Sikender Shahid <sikendershahid91@gmail.com>
2017-11-17 08:47:43 -0600

    3.(b) done

--- design.m
+++ design.m
@@ -7,17 +7,17 @@ damping_ratio = 0.707;
 
 syms s k1 k2 
 
-% TO ERASE 
+% TO BE ERASED------ 
 % motor_sys = 465/(s + 84.8);
 % gear_sys = 1/20; 
 % load_sys = 1/s; 
 % open_loop_sys = (k1 * motor_sys * gear_sys * load_sys)/(1 + motor_sys*k2 + k1 * motor_sys * gear_sys * load_sys);
 % open_loop_sys_damp = natural_frequency^2/(s^2 + 2*damping_ratio*natural_frequency*s+(natural_frequency^2));
-
+% [y1, y2] = solve(open_loop_sys == open_loop_sys_damp, [k1 k2]);
+%-------------------
 
 equation1 = 23.25*k1 == natural_frequency^2; 
 equation2 = 84.8 + 465*k2 == 2 * damping_ratio * natural_frequency; 
-% [y1, y2] = solve(open_loop_sys == open_loop_sys_damp, [k1 k2]);
 [k1, k2] = solve([equation1 equation2], [k1 k2]);
 % calculator values matlab has a roundoff 
 % k1 = 4.30108
@@ -33,6 +33,10 @@ open_loop_sys = (k1 * motor_sys * gear_sys * load_sys)/(1 + motor_sys*k2 + k1 * 
 
 figure(1); 
 step(open_loop_sys); 
+step_info = stepinfo(open_loop_sys); 
 
+step_info
 
-
+%static error 
+%Simulate through block diagrams
+%or from experimentation plot static error 
